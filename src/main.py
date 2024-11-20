73cec6522cc06df85676dd2bf0e61eacdd3902f4
CodingForEntrepreneurs <hello@teamcfe.com>
2024-11-20 16:14:59 -0700

    15 - Rate Limit API Requests with Upstash Redis

--- src/main.py
+++ src/main.py
@@ -1,12 +1,38 @@
+import redis.asyncio as redis
+
+from contextlib import asynccontextmanager
 from typing import Union
 
-from fastapi import FastAPI, HTTPException
+from decouple import config
+from fastapi import (
+    Depends,
+    FastAPI, 
+    HTTPException
+    )
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
+
 from pydantic import BaseModel
+
 import helpers
 
-app = FastAPI()
+REDIS_URL = config('REDIS_URL')
 
-@app.get("/")
+@asynccontextmanager
+async def lifespan(_:FastAPI):
+    redis_conn = redis.from_url(REDIS_URL)
+    await FastAPILimiter.init(
+        redis_conn
+    )
+    yield
+    await FastAPILimiter.close()
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/", dependencies=[
+    Depends(RateLimiter(times=2, seconds=5)),
+    Depends(RateLimiter(times=4, seconds=20))
+])
 def read_root():
     # helpers.generate_image()
     return {"Hello": "World"}
@@ -15,7 +41,12 @@ def read_root():
 class ImageGenerationRequest(BaseModel):
     prompt:str
 
-@app.post('/generate')
+@app.post('/generate', 
+        dependencies=[
+            Depends(RateLimiter(times=2, seconds=5)),
+            Depends(RateLimiter(times=10, minutes=1))
+        ]
+)
 def create_image(data: ImageGenerationRequest):
     try:
         pred_result = helpers.generate_image(data.prompt)
